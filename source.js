6bf5f441b515748d0282940c68e4be59bb64bf4b
karianjahi <kitgaedesigaidi8@gmail.com>
2025-11-19 12:46:25 +0100

    Make comments on user stories and initialize the function that calculates the prime no sum

---
+++ source.js
@@ -0,0 +1,13 @@
+/*
+Building a Prime Number Sum Calculator
+In this lab, we build a calculator that takes a number and returns the sum of all prime numbers that are less than or equal to that number.
+
+User Stories:
+1. We should have a `sumPrimes` function that accepts a number as an argument.
+2. The `sumPrimes` function should return the sum of all prime numbers less than or equal to the provided number.
+3. If the input number is less than 2, the function should return 0.
+*/
+
+const sumPrimes = (num) => {
+    
+}
